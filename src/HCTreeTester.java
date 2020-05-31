38d44716806b5581e916337a559ed9e978c4df13
Albert 孔 <albertkong@Alberts-MBP-4.attlocal.net>
2020-05-30 19:42:47 -0700

    PA9

---
+++ src/HCTreeTester.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+public class HCTreeTester {
+
+    @org.junit.Test
+    public void getRoot() {
+    }
+
+    @org.junit.Test
+    public void setRoot() {
+    }
+
+    @org.junit.Test
+    public void buildTree() {
+        HCTree test1 = new HCTree();
+
+    }
+
+    @org.junit.Test
+    public void encode() {
+    }
+
+    @org.junit.Test
+    public void decode() {
+    }
+
+    @org.junit.Test
+    public void encodeHCTree() {
+    }
+
+    @org.junit.Test
+    public void decodeHCTree() {
+    }
+}
